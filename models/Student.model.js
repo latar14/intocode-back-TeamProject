501fc3c8bd55895d32691e29689e032485b8fac0
Ashwash <latar14@bk.ru>
2022-08-30 11:24:52 +0300

    Сделана модель студента

--- models/Student.model.js
+++ models/Student.model.js
@@ -1,2 +1,16 @@
-//Все поля которые нужны студенту
-//Обязателен ref на группу
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema({
+    name: String,
+    surmane: String,
+    image: String,
+    group: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Group'
+    },
+    email: String,
+    notes: []
+});
+
+const Student = mongoose.model('Student', studentSchema);
+module.exports = Student;
